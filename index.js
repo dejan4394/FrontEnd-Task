54e072a6d486f44cd58b50503f5563469f6f057c
dejan4394 <79114957+dejan4394@users.noreply.github.com>
2021-05-31 03:24:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -21,4 +21,32 @@ const navSlide = ()=>{
   
 }
 
-navSlide();
+
+
+
+const close = ()=>{
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li")
+
+navLinks.forEach(link=>{
+  link.addEventListener("click", ()=> {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+    navLinks.forEach(link=>{
+      link.style.animation = ""
+    })
+})
+})
+}
+
+  
+const app = ()=>{
+  navSlide();
+  close();
+}
+
+
+
+app();
